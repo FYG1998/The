0b332bb719504024680376bd96e7c0f2f04a72b3
admin <1596782257@qq.com>
2021-05-06 20:56:38 +0800

    强制加载X5内核

--- app/src/main/java/com/example/demo/APPAplication.java
+++ app/src/main/java/com/example/demo/APPAplication.java
@@ -20,8 +20,6 @@ public class APPAplication extends Application {
 		super.onCreate();
 		context = this;
 
-
-
 		initData();
 		initView();
 
@@ -32,6 +30,9 @@ public class APPAplication extends Application {
 	}
 
 	private void initView() {
+
+		//非wifi情况下，主动下载x5内核
+		QbSdk.setDownloadWithoutWifi(true);
 		//搜集本地tbs内核信息并上报服务器，服务器返回结果决定使用哪个内核。
 		QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
 
@@ -39,19 +40,17 @@ public class APPAplication extends Application {
 			public void onViewInitFinished(boolean arg0) {
 				// TODO Auto-generated method stub
 				//x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
-				Log.d("调试", " onViewInitFinished is " + arg0);
+				Log.e("调试", "APPAplication初始化X5  " + arg0);
 			}
 
 			@Override
 			public void onCoreInitFinished() {
 				// TODO Auto-generated method stub
-				Log.e("调试", "APPAplication --初始化X5失败 ");
-
+				Log.d("调试", " onCoreInitFinished  回调接口 " );
 			}
 		};
-		Log.e("调试", cb.toString());
 		//x5内核初始化接口
-		QbSdk.initX5Environment(this,  cb);
+		QbSdk.initX5Environment(context,  cb);
 
 	}
 
